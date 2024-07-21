3ffbbd936e30b7aeaa93a4cd50947d5596363031
Tyler Dane <tyler@switchback.tech>
2024-07-21 13:12:32 -0500

    :green_heart: Fix: fix link to Install doc by uppercasing first letter

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/getting-started/install"
+            to="/docs/getting-started/Install"
           >
             Get Started - 23 min ⏱️
           </Link>
