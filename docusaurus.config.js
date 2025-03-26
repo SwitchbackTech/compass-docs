0805b0c02d0998ae6788e648571b64173b937fb9
Tyler Dane <tyler@switchback.tech>
2025-03-26 07:16:05 -0500

    ✨ Feat: enable mermaid

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,6 +23,12 @@ const config = {
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
 
+  markdown: {
+    mermaid: true,
+  },
+
+  themes: ["@docusaurus/theme-mermaid"],
+
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
@@ -113,7 +119,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} Switchback LLC`,
+        copyright: `Copyright © ${new Date().getFullYear()} Switchback Tech LLC`,
       },
       prism: {
         theme: lightCodeTheme,
